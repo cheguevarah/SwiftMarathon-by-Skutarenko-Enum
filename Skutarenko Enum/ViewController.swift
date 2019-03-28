475a1b61fb16146d96678c002098c28faba13dc6
s.) <sashkah@icloud.com>
2019-03-28 14:43:26 +0200

    Initial Commit

---
+++ Skutarenko Enum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skutarenko Enum
+//
+//  Created by Sasha on 3/28/19.
+//  Copyright © 2019 Sasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
